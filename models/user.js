246b6f455c1473f763d70a814c0c018a7f6e8e14
pcreem <creempepper@gmail.com>
2019-11-06 21:02:34 +0800

    first commit

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
+  }, {})
+  User.associate = function (models) {
+    User.hasMany(models.Comment)
+  }
+  return User
+}
