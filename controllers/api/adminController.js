5f46baa7abfef1bd9aefc625eaf90fc9c09ff1f8
pcreem <creempepper@gmail.com>
2019-11-13 16:41:10 +0800

    api

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -9,7 +9,14 @@ const adminController = {
     adminService.getRestaurants(req, res, (data) => {
       return res.json(data)
     })
-  }
+  },
+  getRestaurant: (req, res) => {
+    adminService.getRestaurant(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+
+
 }
 module.exports = adminController
 
--- services/adminService.js
+++ services/adminService.js
@@ -1,11 +1,48 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const User = db.User
+const Comment = db.Comment
 
 const adminService = {
   getRestaurants: (req, res, callback) => {
     return Restaurant.findAll({ include: [Category] }).then(restaurants => {
       callback({ restaurants: restaurants })
+    })
+  },
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: User, as: 'FavoritedUsers' },
+        { model: User, as: 'LikedUsers' },
+        { model: Comment, include: [User] }
+      ]
+    }).then(restaurant => {
+      restaurant.viewCounts += 1
+      restaurant.save()
+        .then(restaurant => {
+          const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
+          const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
+          callback({
+            restaurant: restaurant,
+            isFavorited: isFavorited,
+            isLiked: isLiked
+          })
+        })
+    })
+  },
+
+  getCategories: (req, res, callback) => {
+    return Category.findAll().then(categories => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then((category) => {
+            callback({ categories: categories, category: category })
+          })
+      } else {
+        callback({ categories: categories })
+      }
     })
   }
 }
